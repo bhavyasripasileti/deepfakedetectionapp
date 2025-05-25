486f7f76041313bedd09c536f570abb2b09c4f3c
bhavyasripasileti <bhavyasripasileti@gmail.com>
2025-05-25 14:07:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="deepfake-detection",
     version="0.1",
     install_requires=[
-        'tensorflow-cpu==2.10.0',
+        'tensorflow-cpu==2.12.0',
         'opencv-python-headless==4.6.0.66',
     ],
 )
