41141c0b22b97c48742752aa2f19a088f5fdf8df
bhavyasripasileti <bhavyasripasileti@gmail.com>
2025-05-25 20:08:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import tempfile
 # Load the model once with caching
 @st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("deepfake_model.h5")
+    return tf.keras.models.load_model("new_model.h5")
 
 model = load_model()
 
